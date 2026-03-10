c02c29ff3dc4e6c03de8698a0370579313374aea
luthfimaajid <luthfimaajid@gmail.com>
2026-03-10 09:11:28 +0700

    feat: submit no 2 answer

--- 2/main.sql
+++ 2/main.sql
@@ -1,10 +1,10 @@
 SELECT
-    e.employee_id,
-    e.full_name,
+    CONCAT(e.full_name, ' (', e.employee_id, ') has a performance rating of: ',
     CASE 
         WHEN e.performance_score < 50 THEN 'Needs Improvement'
         WHEN e.performance_score < 75 THEN 'Meets Expectations'
         WHEN e.performance_score < 90 THEN 'Exceeds Expectations'
         ELSE 'Outstanding'
-    END AS performance_rating
-FROM employee e 
+    END)
+FROM employee e
+ORDER BY e.employee_id ASC; 
